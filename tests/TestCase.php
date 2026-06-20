0a4a115ba161787430a18a97abe211bbb796313b
DatPA Dev <datpa12192@gmail.com>
2026-06-20 07:01:24 +0700

    fix(tests): add app.key to TestCase for web feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,6 +18,7 @@ abstract class TestCase extends OrchestraTestCase
 
     protected function defineEnvironment($app): void
     {
+        $app['config']->set('app.key', 'base64:' . base64_encode(random_bytes(32)));
         $app['config']->set('database.default', 'testing');
         $app['config']->set('database.connections.testing', [
             'driver'   => 'sqlite',
